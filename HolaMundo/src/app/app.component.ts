314d5bc30145eab70e5b8fc95d4fb7e0aed2b242
Emiliano Bazan <maximolicanelo@gmail.com>
2021-02-24 03:28:10 +0100

    Property Binding

--- HolaMundo/src/app/app.component.ts
+++ HolaMundo/src/app/app.component.ts
@@ -6,16 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  //interpolacion del archivo ts al html
+  //interpolacion del archivo ts al html INTERPOLACION
   nombre = 'Emiliano'
+  
 
-  constructor()
-  {
-    setInterval(()=> this.nombre = 'Ramiro',3000)
-  }
+  //constructor()
+  //
+    //setInterval(()=> this.nombre = 'Ramiro',3000)
+  //}
 
   getSuma(numero1:number , numero2:number)
   {
     return numero1 + numero2;
+  }
+  //interpolacion del archivo ts al html Binding
+  textoPlaceHolder='Escriba algo Aqui'
+  InputDeshabilitado = true;
+  imgSrc= 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/AngularJS_logo.svg/1280px-AngularJS_logo.svg.png'
+
+  constructor()
+  {
+    setInterval(()=> this.InputDeshabilitado = false,3000)
+    setInterval(()=> this.nombre = 'Ramiro',3000)
   }
 }
