c44fcdfe45eebd7d6491842b6ae191f5f05c273d
Emiliano Bazan <maximolicanelo@gmail.com>
2021-02-25 15:09:54 +0100

    Creando los Componentes

--- Convertidor/src/app/app.module.ts
+++ Convertidor/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { ConvertidorComponent } from './Components/convertidor/convertidor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ConvertidorComponent
   ],
   imports: [
     BrowserModule
