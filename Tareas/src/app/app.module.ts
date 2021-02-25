b5ed54abe71ce759866d252a91888520dc076850
Emiliano Bazan <maximolicanelo@gmail.com>
2021-02-25 13:20:11 +0100

    Creando componentes : Tareas  y NavBar

--- Tareas/src/app/app.module.ts
+++ Tareas/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { TareasComponent } from './Components/tareas/tareas.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    TareasComponent
   ],
   imports: [
     BrowserModule
