afa22ebb2656c887bb7ccd909935c7e23194ba8c
Emiliano Bazan <maximolicanelo@gmail.com>
2021-02-25 13:04:21 +0100

    Creando Proyecyo : Tareas

--- Dados/src/app/app.component.ts
+++ Dados/src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dados';
+  dadoIzquierda='../assets/Img/dice1.png';
+  dadoDerecha='../assets/Img/dice1.png';
+  numero1 = 3;
+  numero2 = 5;
+
+  TirarDados():void
+  {
+    this.numero1 = Math.round(Math.random() * 5)+1;
+    this.numero2 = Math.round(Math.random() * 5)+1;
+    this.dadoIzquierda ='../assets/Img/dice'+ this.numero1 +'.png';
+    this.dadoDerecha ='../assets/Img/dice'+ this.numero2 +'.png';
+  }
 }
