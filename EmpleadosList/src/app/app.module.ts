0627c1a87d8ff628f510468eb8cb71be845bef83
Emiliano Bazan <maximolicanelo@gmail.com>
2021-02-26 16:54:54 +0100

    Creacion de Componentes y Agregando Fondo

--- EmpleadosList/src/app/app.module.ts
+++ EmpleadosList/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EmpleadoListComponent } from './Components/empleado-list/empleado-list.component';
+import { CountEmpleadosComponent } from './Components/Empleado-List/count-empleados/count-empleados.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleadoListComponent,
+    CountEmpleadosComponent
   ],
   imports: [
     BrowserModule
