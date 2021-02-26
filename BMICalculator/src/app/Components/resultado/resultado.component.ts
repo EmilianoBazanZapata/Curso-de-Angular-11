e9df5e1e5ce73aa1e5d01c4ad7b8cd3ed22b7c2b
Emiliano Bazan <maximolicanelo@gmail.com>
2021-02-26 14:30:20 +0100

    Mostramos el Resultado y  los mensajes

--- BMICalculator/src/app/Components/resultado/resultado.component.ts
+++ BMICalculator/src/app/Components/resultado/resultado.component.ts
@@ -9,12 +9,34 @@ import { ActivatedRoute } from '@angular/router';
 export class ResultadoComponent implements OnInit {
 
   bmi: number;
+  resultado:string;
+  interpretacion:string;
   constructor(private route:ActivatedRoute) { 
+    this.resultado='';
+    this.interpretacion='';
     this.bmi = +route.snapshot.paramMap.get('valor')!;
   }
 
   ngOnInit(): void {
+    this.getResultado();
   }
-  
+  getResultado()
+  {
+    if(this.bmi >=25)
+    {
+      this.resultado = 'Exceso de Peso';
+      this.interpretacion = 'Tienes un Peso corporal al Superior . Intente hacer mas ejercicio';
+    }
+    else if(this.bmi >=18.5)
+    {
+      this.resultado = 'Normal';
+      this.interpretacion = 'Tienes un buen peso corporal. ¡Buen Trabajo!';
+    }
+    else
+    {
+      this.resultado = 'Bajo Peso';
+      this.interpretacion = 'Tienes un peso Corporal mas bajo de lo normal . ¡Puedes comer como CERDO!';
+    }
+  }
 
 }
