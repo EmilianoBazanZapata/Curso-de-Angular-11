cf010292a8441dd17d1712b085577c7941319e04
Emiliano Bazan <maximolicanelo@gmail.com>
2021-02-28 19:08:03 +0100

    Generando Componentes y Servicios

--- Presupuesto/src/app/app.module.ts
+++ Presupuesto/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GastosComponent } from './Components/gastos/gastos.component';
+import { IngresarPresupuestoComponent } from './Components/ingresar-presupuesto/ingresar-presupuesto.component';
+import { IngresarGastoComponent } from './Components/Gastos/ingresar-gasto/ingresar-gasto.component';
+import { ListarGastoComponent } from './Components/Gastos/listar-gasto/listar-gasto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GastosComponent,
+    IngresarPresupuestoComponent,
+    IngresarGastoComponent,
+    ListarGastoComponent
   ],
   imports: [
     BrowserModule,
