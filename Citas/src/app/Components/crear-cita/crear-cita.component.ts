0b8dfb0f915c42f38b54528ff9acadd0f548576e
Emiliano Bazan <maximolicanelo@gmail.com>
2021-02-28 04:43:54 +0100

    Crear Cita TS

--- Citas/src/app/Components/crear-cita/crear-cita.component.ts
+++ Citas/src/app/Components/crear-cita/crear-cita.component.ts
@@ -6,10 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crear-cita.component.css']
 })
 export class CrearCitaComponent implements OnInit {
+  Nombre='';
+  Fecha='';
+  Hora='';
+  Sintomas='';
 
+  FormularioIncorrecto =false;
   constructor() { }
 
   ngOnInit(): void {
   }
+  AgregarCita()
+  {
+    if(this.Nombre ==''|| this.Fecha =='' || this.Hora =='' || this.Sintomas=='')
+    {
+      this.FormularioIncorrecto = true
+      return;
+    }
+    this.FormularioIncorrecto = false;
+
+    //creamos un objeto para pasarlo al padre
+
+    const Cita =
+    {
+      nombre: this.Nombre,
+      fecha: this.Fecha,
+      hora: this.Hora,
+      sintomas: this.Sintomas
+      
+    }
+    this.ResetCampos();
+    
+  }
+  ResetCampos()
+    {
+      this.Nombre='',
+      this.Hora ='',
+      this.Fecha = '',
+      this.Sintomas=''
+    }
 
 }
--- Citas/src/app/app.module.ts
+++ Citas/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ListCitasComponent } from './Components/list-citas/list-citas.component';
 import { CrearCitaComponent } from './Components/crear-cita/crear-cita.component';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { CrearCitaComponent } from './Components/crear-cita/crear-cita.component
     CrearCitaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
