f948ab9f3654c299bddb10bc44a32cbf197230b5
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-03 00:21:38 +0100

    Agregando el link con los parametros al Service

--- Clima/src/app/Services/clima.service.ts
+++ Clima/src/app/Services/clima.service.ts
@@ -1,11 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClimaService {
 
-  Url='';
+  Url='http://api.openweathermap.org/data/2.5/weather?&appid=';
   Key='5754e3ef9c214e608e57faad2486bd70';
-  constructor() { }
+  constructor(private http:HttpClient) 
+  {
+
+  }
+  GetClima(ciudad:string): Observable<any>
+  {
+    //concateno la url para hacer la solicitud get 
+    const Url = this.Url + this.Key + '&q' + ciudad;
+
+    return this.http.get(Url);
+
+  }
 }
--- Clima/src/app/app.module.ts
+++ Clima/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
@@ -15,7 +16,8 @@ import { DashBoardComponent } from './Components/dash-board/dash-board.component
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
