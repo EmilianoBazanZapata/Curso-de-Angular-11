4668f057da82b3951eae41ab4372e08af439769c
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-02 22:49:08 +0100

    Agregando Componentes y Servicios

--- Clima/src/app/app.module.ts
+++ Clima/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { DashBoardComponent } from './Components/dash-board/dash-board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    DashBoardComponent
   ],
   imports: [
     BrowserModule
