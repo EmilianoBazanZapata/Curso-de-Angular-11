ae1a8263b78f23febdf69fb420dec30d03a27d78
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-04 01:23:19 +0100

    Generando Componentes y Servicios

--- ShortUrl/src/app/app.module.ts
+++ ShortUrl/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { ShortUrlComponent } from './Components/short-url/short-url.component';
+import { SpinnerComponent } from './Components/spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    ShortUrlComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule
