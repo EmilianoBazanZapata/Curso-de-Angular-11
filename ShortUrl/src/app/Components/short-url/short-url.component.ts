a60a279e627d18f72b30429e71f974023b4b78c1
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-04 04:03:18 +0100

    Validando URL Vacia

--- ShortUrl/src/app/Components/short-url/short-url.component.ts
+++ ShortUrl/src/app/Components/short-url/short-url.component.ts
@@ -8,31 +8,47 @@ import { ShortUrlService } from 'src/app/Services/short-url.service';
 })
 export class ShortUrlComponent implements OnInit {
 
-  NombreUrl:string;
-  UrlShort:string;
-  UrlProcesada:boolean;
-  Loading:boolean;
-  constructor(private _ShortUrlServices:ShortUrlService) 
-  {
+  NombreUrl: string;
+  UrlShort: string;
+  UrlProcesada: boolean;
+  Loading: boolean;
+  MostrarError: boolean;
+  constructor(private _ShortUrlServices: ShortUrlService) {
     this.NombreUrl = '';
     this.UrlShort = '';
     this.UrlProcesada = false;
     this.Loading = false;
+    this.MostrarError = false;
   }
 
   ngOnInit(): void {
   }
-  ProcesarUrl(){
+  ProcesarUrl() {
     this.Loading = true;
     this.UrlProcesada = false;
 
+    //a los 3 segundo muestro el resultado y dejo en blanco el textbox
     setTimeout(() => {
-      this.ObtenerUrlShort();
-    },5000);
+      //validar URL vacia
+      if (this.NombreUrl === '') {
+        this.MostrarError = true;
+        this.Loading = false;
+        //a los 4 segundos quito la advertencia del campo vacio 
+        setTimeout(() => {
+          this.MostrarError = false;
+        }, 4000);
+        return;
+      }
+      else {
+        this.ObtenerUrlShort();
+        this.NombreUrl = '';
+      }
+
+    }, 3000);
   }
-  ObtenerUrlShort()
-  {
-    this._ShortUrlServices.GetUrlShort(this.NombreUrl).subscribe(data =>{
+  ObtenerUrlShort() {
+
+    this._ShortUrlServices.GetUrlShort(this.NombreUrl).subscribe(data => {
       this.UrlProcesada = true;
       this.UrlShort = data.link;
       this.Loading = false;
