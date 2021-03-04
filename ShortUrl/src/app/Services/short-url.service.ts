c98ae74648ccdcccb3c3e895a2c52073b668c5d1
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-04 03:12:25 +0100

    Generndo Token desde Bitly

--- ShortUrl/src/app/Services/short-url.service.ts
+++ ShortUrl/src/app/Services/short-url.service.ts
@@ -5,5 +5,7 @@ import { Injectable } from '@angular/core';
 })
 export class ShortUrlService {
 
+  Url ='https://api-ssl.bitly.com/v4/shorten';
+  Token='f01bdf15e54e6c20ed61f6eb105216f5ef36c3ad';
   constructor() { }
 }
