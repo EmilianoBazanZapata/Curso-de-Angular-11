967eed228ca1915beea58f7e4641e3c77c3c0834
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-04 04:27:17 +0100

    mostrando mensajes de error desde el interceptor

--- ShortUrl/src/app/Services/short.interceptor.ts
+++ ShortUrl/src/app/Services/short.interceptor.ts
@@ -3,19 +3,25 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 
 @Injectable()
 export class ShortInterceptor implements HttpInterceptor {
 
   constructor() {}
-
+//muestro los errores
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const Token ='f01bdf15e54e6c20ed61f6eb105216f5ef36c3ad';
 
     request = request.clone({setHeaders:{Authorization: 'Bearer '+ Token}})
-    return next.handle(request);
+    return next.handle(request).pipe(catchError((error:HttpErrorResponse)=>
+    {
+      console.log(error);
+      return throwError(error);
+    }));
   }
 }
