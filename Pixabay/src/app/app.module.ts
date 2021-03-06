8618a593094a2c6510b26541e005e35b502b4965
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-07 00:19:09 +0100

    Creando Componentes y Servicios

--- Pixabay/src/app/app.module.ts
+++ Pixabay/src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { BuscarImagenComponent } from './Components/buscar-imagen/buscar-imagen.component';
+import { ListarImagenComponent } from './Components/listar-imagen/listar-imagen.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { ErrorComponent } from './Shared/error/error.component';
+import { SpinnerComponent } from './Shared/spinner/spinner.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuscarImagenComponent,
+    ListarImagenComponent,
+    NavBarComponent,
+    ErrorComponent,
+    SpinnerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
