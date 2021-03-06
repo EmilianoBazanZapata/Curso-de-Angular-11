34bf80edc17ea4303448f73d6d0a7d004c42dd7c
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-06 22:52:20 +0100

    Agregando Arraylist con opciones en el Formulario

--- Noticias/src/app/Components/formulario/formulario.component.ts
+++ Noticias/src/app/Components/formulario/formulario.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioComponent implements OnInit {
 
+  CategoriaSeleccionada='general';
+  PaisSeleccionado='ar';
+  categorias :any[]=
+  [
+    {value:'general' , nombre:'General'},
+    {value:'business' , nombre:'Negocios'},
+    {value:'entretainment' , nombre:'Entretenimiento'},
+    {value:'health' , nombre:'Salud'},
+    {value:'science' , nombre:'Ciencia'},
+    {value:'sports' , nombre:'Deportes'},
+    {value:'technology' , nombre:'Tecnologia'}
+  ];
+
+  paises:any[]=
+  [
+    {value:'ar' , nombre:'Argentina'},
+    {value:'br' , nombre:'Brasil'},
+    {value:'fr' , nombre:'Francia'},
+    {value:'hu' , nombre:'Hungaria'},
+    {value:'bmx' , nombre:'Mexico'},
+    {value:'gb' , nombre:'Reino Unido'}
+  ];
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  BuscarNoticia()
+  {
+    console.log(this.PaisSeleccionado);
+    console.log(this.CategoriaSeleccionada);
+  }
 }
