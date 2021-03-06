5c967457eeff3b662aa77eca28c22c91e90342f6
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-07 00:24:26 +0100

    Agregando el NavBar con HTML

--- Pixabay/src/app/Components/nav-bar/nav-bar.component.ts
+++ Pixabay/src/app/Components/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavBarComponent implements OnInit {
 
-  constructor() { }
+  @Input() Titulo : string;
+  constructor(){
+    this.Titulo = ''; 
+ }
 
   ngOnInit(): void {
   }
