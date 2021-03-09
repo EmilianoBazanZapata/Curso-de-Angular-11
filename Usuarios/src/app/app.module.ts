fe8a763a9efe202c3503a68104bbb889a4db78ab
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-09 21:01:06 +0100

    Creando componentes

--- Usuarios/src/app/app.module.ts
+++ Usuarios/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { FooterComponent } from './dash-board/footer/footer.component';
+import { NavBarComponent } from './dash-board/nav-bar/nav-bar.component';
+import { ListUsuariosComponent } from './dash-board/list-usuarios/list-usuarios.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashBoardComponent,
+    FooterComponent,
+    NavBarComponent,
+    ListUsuariosComponent
   ],
   imports: [
     BrowserModule,
