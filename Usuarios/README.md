9064fa5aff80c3c391d67fd05996cfc4e7f79090
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-09 20:45:33 +0100

    Creacion del Proyecto : Usuarios

--- Usuarios/README.md
+++ Usuarios/README.md
@@ -1,4 +1,4 @@
-# RuteoNavegacion
+# Usuarios
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 11.2.1.
 
--- Usuarios/src/app/app.component.spec.ts
+++ Usuarios/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'RuteoNavegacion'`, () => {
+  it(`should have as title 'Usuarios'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('RuteoNavegacion');
+    expect(app.title).toEqual('Usuarios');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('RuteoNavegacion app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Usuarios app is running!');
   });
 });
