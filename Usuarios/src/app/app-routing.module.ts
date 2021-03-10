0ba1b0177fe73bd6525ada90ffead0899245eaa5
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-10 03:25:47 +0100

    Ruteo de las Paginas

--- Usuarios/src/app/app-routing.module.ts
+++ Usuarios/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'layout' , component:LayoutComponent},
+  {path: 'dashboard' , component:DashBoardComponent},
+  {path: '' , redirectTo :'layout' , pathMatch:'full'},
+  {path: '**' , redirectTo :'layout' , pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
