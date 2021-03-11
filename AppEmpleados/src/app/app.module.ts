a586f41c7a0aafb7e23bef98291228c00ef52dee
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-12 00:55:38 +0100

    Creacion de Componentes y Servicios

--- AppEmpleados/src/app/app.module.ts
+++ AppEmpleados/src/app/app.module.ts
@@ -4,17 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSliderModule } from '@angular/material/slider';
+
+import { AddEditEmpleadoComponent } from './Components/add-edit-empleado/add-edit-empleado.component';
+import { ListEmpleadoComponent } from './Components/list-empleado/list-empleado.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { MensajeConfirmacionComponent } from './Components/Shared/mensaje-confirmacion/mensaje-confirmacion.component';
+import { AngularMaterialModule } from './Components/Shared/angular-material/angular-material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddEditEmpleadoComponent,
+    ListEmpleadoComponent,
+    NavBarComponent,
+    MensajeConfirmacionComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    AngularMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
