6572c496208103142a89a8e619936190a0af80eb
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-12 04:44:02 +0100

    Mostrar mensaje para confirmar la eliminacion

--- AppEmpleados/src/app/Components/list-empleado/list-empleado.component.ts
+++ AppEmpleados/src/app/Components/list-empleado/list-empleado.component.ts
@@ -1,59 +1,69 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-import {MatPaginator} from '@angular/material/paginator';
+import { MatPaginator } from '@angular/material/paginator';
 import { AfterViewInit } from '@angular/core';
 import { MatSort } from '@angular/material/sort';
 import { EmpleadoService } from 'src/app/Services/empleado.service';
 import { Empleado } from 'src/app/Models/Empleado';
+import { MatDialog } from '@angular/material/dialog';
+import { MensajeConfirmacionComponent } from '../Shared/mensaje-confirmacion/mensaje-confirmacion.component';
 
 @Component({
   selector: 'app-list-empleado',
   templateUrl: './list-empleado.component.html',
   styleUrls: ['./list-empleado.component.css']
 })
-export class ListEmpleadoComponent implements AfterViewInit , OnInit{
+export class ListEmpleadoComponent implements AfterViewInit, OnInit {
 
 
-  displayedColumns: string[] = ['Nombre', 'Correo', 'EstadoCivil', 'FechadeIngreso','Sexo' ,'Telefono','Acciones'];
+  displayedColumns: string[] = ['Nombre', 'Correo', 'EstadoCivil', 'FechadeIngreso', 'Sexo', 'Telefono', 'Acciones'];
   //dataSource = ELEMENT_DATA;
   dataSource = new MatTableDataSource();
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
   //Creo un arregl ode empleados vacia
-  ListEmpleado:Empleado[];
+  ListEmpleado: Empleado[];
 
-  constructor(private _EmpleadoService:EmpleadoService) 
-  {
+  constructor(private _EmpleadoService: EmpleadoService,
+    public dialog: MatDialog) {
 
   }
 
-  ngOnInit():void
-  {
+  ngOnInit(): void {
     this.CargarEmpleados();
   }
   ngAfterViewInit() {
+
     
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
   }
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
-  CargarEmpleados()
-  {
+  CargarEmpleados() {
     //relleno la lista con el listado del servicio
     this.ListEmpleado = this._EmpleadoService.GetEmpleados();
     //paso los datros al data source
     this.dataSource = new MatTableDataSource(this.ListEmpleado);
-    console.log(this.ListEmpleado);
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
   }
-  EliminarEmpleado(id:number)
-  {
-    //elimino el empleado enviando el id al service 
-    this._EmpleadoService.EliminarEmpleado(id);
-    //recargo la ista
-    this.CargarEmpleados();
+  EliminarEmpleado(id: number) {
+    const dialogRef = this.dialog.open(MensajeConfirmacionComponent, {
+      width: '400px',
+      data: { mensaje: 'Esta Seguro de eliminar este Empleado ? ' }
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      if(result === 'Aceptar')
+      {
+        //elimino el empleado enviando el id al service 
+      this._EmpleadoService.EliminarEmpleado(id);
+      //recargo la ista
+      this.CargarEmpleados();
+      }
+      
+    });
+
   }
 }
