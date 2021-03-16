6ae1757a248e3f544c96cb17b06ee454c7cf7ccb
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-16 16:01:49 +0100

    Ruteo de las paginas

--- AppEmpleados/src/app/app-routing.module.ts
+++ AppEmpleados/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddEditEmpleadoComponent } from './Components/add-edit-empleado/add-edit-empleado.component';
+import { ListEmpleadoComponent } from './Components/list-empleado/list-empleado.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'add',component:AddEditEmpleadoComponent},
+  {path:'',component:ListEmpleadoComponent},
+  {path:'edit/:id' ,component:AddEditEmpleadoComponent},
+  {path:'**',component:ListEmpleadoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
