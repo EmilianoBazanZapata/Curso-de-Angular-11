f0b5e8babe3ca6de6b60a70309e2f6112bae3d85
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-17 10:46:35 -0300

    Configurando el Routing

--- AppPreguntasRespuestas/src/app/app-routing.module.ts
+++ AppPreguntasRespuestas/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashBoardComponent } from './Components/dash-board/dash-board.component';
+import { PreguntaComponent } from './Components/pregunta/pregunta.component';
+import { RespuestaComponent } from './Components/respuesta/respuesta.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'dashboard',component:DashBoardComponent},
+  {path:'pregunta',component:PreguntaComponent},
+  {path:'respuesta',component:RespuestaComponent},
+  {path:'',redirectTo:'/dashboard', pathMatch:'full'},
+  {path:'**',redirectTo:'/dashboard', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
