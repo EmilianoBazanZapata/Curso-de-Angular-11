5a4275588afb8d706892c22283723513cb777f6b
Emiliano Bazan <maximolicanelo@gmail.com>
2021-03-17 01:43:03 -0300

    Creando Componentes y Servicios

--- AppPreguntasRespuestas/src/app/app.module.ts
+++ AppPreguntasRespuestas/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashBoardComponent } from './Components/dash-board/dash-board.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { PreguntaComponent } from './Components/pregunta/pregunta.component';
+import { RespuestaComponent } from './Components/respuesta/respuesta.component';
+import { BotoneraComponent } from './Components/Pregunta/botonera/botonera.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashBoardComponent,
+    NavBarComponent,
+    PreguntaComponent,
+    RespuestaComponent,
+    BotoneraComponent
   ],
   imports: [
     BrowserModule,
